159817cea2051708ebe65ee8ac46813b36158474
Asharam Khatik <84329079+asharam582@users.noreply.github.com>
2024-05-15 03:34:20 +0530

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -17,11 +17,11 @@ cursor.execute(table_info)
 
 ## Insert Some more records
 
-cursor.execute('''Insert Into STUDENT values('Krish','Data Science','A',90)''')
-cursor.execute('''Insert Into STUDENT values('Sudhanshu','Data Science','B',100)''')
-cursor.execute('''Insert Into STUDENT values('Darius','Data Science','A',86)''')
-cursor.execute('''Insert Into STUDENT values('Vikash','DEVOPS','A',50)''')
-cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A',35)''')
+cursor.execute('''Insert Into STUDENT values('ravish','Data Science','A',80)''')
+cursor.execute('''Insert Into STUDENT values('anshu','Data Science','e',10)''')
+cursor.execute('''Insert Into STUDENT values('Dari','Data Science','b',66)''')
+cursor.execute('''Insert Into STUDENT values('kash','DEVOPS','c',50)''')
+cursor.execute('''Insert Into STUDENT values('ipesh','DEVOPS','d',35)''')
 
 ## Disspaly ALl the records
 
